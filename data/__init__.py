bbe3497cfb1f9c73480a79ae62e98b39f80819d7
wangfin <769790863@qq.com>
2020-08-26 15:58:54 +0800

    创建项目结构

---
+++ data/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# @Time    : 2020/8/26 15:53
+# @Author  : wb
+# @File    : __init__.py.py
---
+++ models/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# @Time    : 2020/8/26 15:54
+# @Author  : wb
+# @File    : __init__.py.py
