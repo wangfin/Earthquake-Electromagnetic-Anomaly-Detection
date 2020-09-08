7ac42ee08ba1f3257f9e40831287f972835c2682
wangfin <769790863@qq.com>
2020-09-08 15:56:24 +0800

    添加train文件

--- config.py
+++ config.py
@@ -15,16 +15,16 @@ class DefaultConfig(object):
     model = 'MSNet'  # 使用的模型，名字必须与models/__init__.py中的名字一致
 
     train_data_root = './data/train/'  # 训练集存放路径
-    test_data_root = './data/test'  # 测试集存放路径
-    load_model_path = 'checkpoints/model.pth'  # 加载预训练的模型的路径，为None代表不加载
+    test_data_root = './data/test1'  # 测试集存放路径
+    load_model_path = './checkpoints/model.pth'  # 加载预训练的模型的路径，为None代表不加载
 
     batch_size = 128  # batch size
     use_gpu = True  # user GPU or not
     num_workers = 4  # how many workers for loading data
     print_freq = 20  # print info every N batch
 
-    debug_file = '/tmp/debug'  # if os.path.exists(debug_file): enter ipdb
-    result_file = 'result.csv'
+    debug_file = './tmp/debug'  # if os.path.exists(debug_file): enter ipdb
+    result_file = './results/result.csv'
 
     max_epoch = 10
     lr = 0.1  # initial learning rate
